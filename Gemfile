a7297376ea46a0b0ec856e4cd7e038f5941e546c
ttenneBnaeS <seanerbennett@gmail.com>
2020-01-30 17:28:25 -0600

    HW Done

--- Gemfile
+++ Gemfile
@@ -2,3 +2,9 @@ source 'https://rubygems.org'
 ruby '>= 2.2.0', '< 3.0'
 
 gem 'rspec', '~> 3.8'
+
+group :development do
+  gem 'guard'
+  gem 'guard-rspec'
+  gem 'pry'
+end
--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,33 +3,62 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  sum = 0
+  for x in arr
+    sum += x
+  end
+  return sum
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  return sum(arr.max(2))
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  z = 0
+  for x in arr
+    z+=1
+    arr[z..-1].each do |y|
+      if x + y == n
+        return true
+      end
+    end
+  end
+  return false
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  return "Hello, #{name}"
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  return s[0,1] =~ /[b-df-hj-np-tv-zB-DF-HJ-NP-TV-Z]/
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  if s =~ /^[0-1]{1,}$/
+    return s.to_i(2)%4 == 0
+  else
+    return false
+  end
 end
 
 # Part 3
 
-class BookInStock
-# YOUR CODE HERE
+class BookInStock attr_accessor :isbn, :price
+  def initialize(isbn, price)
+    raise ArgumentError.new(
+      "Must Provide isbn and price must be greater thatn 0"
+    ) if isbn == '' || price <= 0
+    
+    @isbn = isbn
+    @price = price
+  end
+
+  def price_as_string
+    sprintf('%.2f', price)
+    return "$#{sprintf('%.2f', @price)}"
+  end
 end
